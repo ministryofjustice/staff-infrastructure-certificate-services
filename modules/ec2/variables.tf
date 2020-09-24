671a37d3c79ffa889b203a0179964c0b909ee2e6
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-09-24 09:09:11 +0100

    added ec2 module with alpine ami for network testing

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,51 @@
+variable "prefix" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of instances to launch"
+  type        = number
+  default     = 1
+}
+
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+}
+
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "user_data" {
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
+  type        = string
+  default     = null
+}
+
+variable "key_name" {
+  description = "The name of key pair"
+  type        = string
+  default     = ""
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of security group IDs to associate with"
+  type        = list(string)
+  default     = null
+}
