f144b7103b718bfbb9a0bab6d0fcc506a9e46764
Scott Edwards <scotteza@gmail.com>
2020-09-24 15:40:07 +0100

    Output public key in pem format

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "public_key" {
   value = module.test_key_pair.public_key
 }
 
+output "public_key_pem_format" {
+  value = module.test_key_pair.public_key_pem_format
+}
+
 output "public_ip" {
   value = module.ec2_alpine_public.public_ip
 }
