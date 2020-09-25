c1dddd91665df13f8e6bf3e222c7487fb168ee2c
Scott Edwards <scotteza@gmail.com>
2020-09-25 11:17:22 +0100

    Output key pair private key

--- modules/key_pair/outputs.tf
+++ modules/key_pair/outputs.tf
@@ -6,6 +6,10 @@ output "public_key_pem_format" {
   value = tls_private_key.this.public_key_pem
 }
 
+output "private_key_pem_format" {
+  value = tls_private_key.this.private_key_pem
+}
+
 output "key_name" {
   value = module.key_pair.this_key_pair_key_name
 }
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "public_key_pem_format" {
   value = module.test_key_pair.public_key_pem_format
 }
 
+output "private_key_pem_format" {
+  value = module.test_key_pair.private_key_pem_format
+}
+
 output "public_ip" {
   value = module.ec2_alpine_public.public_ip
 }
