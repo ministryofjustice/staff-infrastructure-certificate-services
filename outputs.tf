99661deb6ea16612c483d9ac7408a99b6cfadeb0
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-09-28 09:38:40 +0100

    Disabled output referencing public_certificate_authority_gateway_ip

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "private_key_pem_format" {
   value = module.test_key_pair.private_key_pem_format
 }
 
-output "public_certificate_authority_gateway_ip" {
-  value = module.public_certificate_authority_gateway.public_ip
-}
+# output "public_certificate_authority_gateway_ip" {
+#   value = module.public_certificate_authority_gateway.public_ip
+# }
 
 output "public_registration_authority_front_end_ip" {
   value = module.public_registration_authority_front_end.public_ip
