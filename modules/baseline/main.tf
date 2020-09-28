ef1099fbfae000562311673b42bb4a8b3802501a
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-09-28 19:49:24 +0100

    Add framework for security groups with ingress rules

--- modules/baseline/main.tf
+++ modules/baseline/main.tf
@@ -34,8 +34,186 @@ module "test_key_pair" {
 module "test_ssh_sg" {
   source = ".././sg"
 
-  vpc_id              = module.test_vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  vpc_id = module.test_vpc.vpc_id
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "public_certificate_authority_gateway_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "http"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "public_registration_authority_front_end_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "https"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 20443
+      to_port     = 20443
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 21443
+      to_port     = 21443
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22443
+      to_port     = 22443
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 8030
+      to_port     = 8030
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "public_bastion_host_windows_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "private_issuing_certificate_authority_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "https"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 1443
+      to_port     = 1443
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 709
+      to_port     = 709
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 710
+      to_port     = 710
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 829
+      to_port     = 829
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "private_registration_authority_back_end_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "private_directory_server_sg" {
+  source = ".././sg"
+
+  vpc_id = module.test_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 
   prefix = var.prefix
   tags   = var.tags
@@ -57,11 +235,10 @@ module "public_certificate_authority_gateway" {
 module "public_registration_authority_front_end" {
   source = ".././ec2"
 
-  ami                    = local.rhel_7_6_x64
-  instance_type          = "t2.micro"
-  subnet_id              = module.test_vpc.public_subnets[0]
-  key_name               = module.test_key_pair.key_name
-  vpc_security_group_ids = [module.test_ssh_sg.this_security_group_id]
+  ami           = local.rhel_7_6_x64
+  instance_type = "t2.micro"
+  subnet_id     = module.test_vpc.public_subnets[0]
+  key_name      = module.test_key_pair.key_name
 
   name = "${var.prefix}-ra-front-end"
   tags = var.tags
@@ -70,11 +247,10 @@ module "public_registration_authority_front_end" {
 module "public_bastion_host_windows" {
   source = ".././ec2"
 
-  ami                    = local.windows_2019_x64
-  instance_type          = "t2.micro"
-  subnet_id              = module.test_vpc.public_subnets[0]
-  key_name               = module.test_key_pair.key_name
-  vpc_security_group_ids = [module.test_ssh_sg.this_security_group_id]
+  ami           = local.windows_2019_x64
+  instance_type = "t2.micro"
+  subnet_id     = module.test_vpc.public_subnets[0]
+  key_name      = module.test_key_pair.key_name
 
   name = "${var.prefix}-bastion-host"
   tags = var.tags
--- modules/sg/outputs.tf
+++ modules/sg/outputs.tf
@@ -1,4 +1,4 @@
 output "this_security_group_id" {
   description = "The ID of the security group"
-  value = module.ssh_security_group.this_security_group_id
+  value       = module.security_group.this_security_group_id
 }
