14b10b9673fc132225741bd9cad19e729233d422
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-09-29 12:41:10 +0100

    Amended output names

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,11 @@ output "prod_private_key_pem_format" {
   value = module.baseline_test_production.private_key_pem_format
 }
 
-output "pre_prod_public_ec2_ca_gw_ip" {
+output "pre_prod_ec2_ca_gw_ip" {
   value = module.baseline_test_pre_production.ec2_ca_gw_ip
 }
 
-output "prod_public_ec2_ca_gw_ip" {
+output "prod_ec2_ca_gw_ip" {
   value = module.baseline_test_production.ec2_ca_gw_ip
 }
 
