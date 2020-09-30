caf802f0cd3b911f4de7b8cf20aaa10771cc8c8c
Scott Edwards <scotteza@gmail.com>
2020-09-30 15:02:58 +0100

    Create separate file for public subnet resources

--- modules/baseline/main.tf
+++ modules/baseline/main.tf
@@ -58,26 +58,6 @@ module "test_ssh_sg" {
   tags   = var.tags
 }
 
-module "sg_bastion_host" {
-  source = ".././sg"
-
-  vpc_id = module.test_vpc.vpc_id
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 3389
-      to_port     = 3389
-      protocol    = "tcp"
-      description = "Remote desktop from the public Internet"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-  egress_with_cidr_blocks = []
-
-  prefix = var.prefix
-  tags   = var.tags
-}
-
 module "sg_ca_gw" {
   source = ".././sg"
 
@@ -312,26 +292,6 @@ module "sg_directory" {
 
   prefix = var.prefix
   tags   = var.tags
-}
-
-module "ec2_bastion_host" {
-  source = ".././ec2"
-
-  ami           = local.ami_windows_2019_x64
-  instance_type = "t2.micro"
-  subnet_id     = module.test_vpc.public_subnets[0]
-  # private_ip             = local.ip_bastion_host
-  key_name               = module.test_key_pair.key_name
-  vpc_security_group_ids = [module.sg_bastion_host.this_security_group_id]
-  get_password_data      = true
-
-  name = "${var.prefix}-bastion-host"
-  tags = var.tags
-}
-
-resource "aws_eip" "ec2_bastion_eip" {
-  instance = module.ec2_bastion_host.instance_id[0]
-  vpc      = true
 }
 
 module "ec2_ca_gw" {
