1433c9ae8a6e8789df21dde374049fe9b2527e07
Scott Edwards <scotteza@gmail.com>
2020-10-01 12:38:58 +0100

    Shift hosts to their own files

---
+++ modules/baseline/host-ldap.tf
@@ -0,0 +1,34 @@
+module "sg_directory" {
+  source = ".././sg"
+
+  vpc_id = module.pki_vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 389
+      to_port     = 389
+      protocol    = "tcp"
+      description = ""
+      cidr_blocks = "0.0.0.0/0" // TODO: We should limit this to CA GW and Issuing CA only
+    },
+  ]
+  egress_with_cidr_blocks = []
+
+  prefix = var.prefix
+  tags   = var.tags
+}
+
+module "ec2_directory" {
+  source = ".././ec2"
+
+  ami           = local.ami_rhel_7_6_x64
+  instance_type = "t2.micro"
+  subnet_id     = module.pki_vpc.private_subnets[1]
+  # private_ip             = local.ip_directory
+  key_name                    = module.test_key_pair.key_name
+  vpc_security_group_ids      = [module.sg_directory.this_security_group_id]
+  associate_public_ip_address = false
+
+  name = "${var.prefix}-directory"
+  tags = var.tags
+}
