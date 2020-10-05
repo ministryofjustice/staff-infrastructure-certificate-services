5f45aa17d3a2a56ba9acae803f3da9ab86d94cb2
Scott Edwards <scotteza@gmail.com>
2020-10-05 11:42:44 +0100

    Add IP address text file output to password-fetcher script

--- get-bastion-box-passwords.sh
+++ get-bastion-box-passwords.sh
@@ -28,13 +28,15 @@ openssl rsautl -decrypt -in ./pre_prod_password_encrypted.bin -out ./prep_passwo
 openssl rsautl -decrypt -in ./prod_password_encrypted.bin -out ./prod_password_decrypted.txt -inkey ./prod_key_pair.pem
 
 printf "\nPREP IP:\n"
-aws-vault exec moj-pttp-pki -- terraform output pre_prod_ec2_bastion_public_ip
-printf "PREP password:\n"
+aws-vault exec moj-pttp-pki -- terraform output pre_prod_ec2_bastion_public_ip > pre_prod_ip_address.txt
+cat pre_prod_ip_address.txt
+printf "\nPREP password:\n"
 cat prep_password_decrypted.txt
 
 printf "\n\nPROD IP:\n"
-aws-vault exec moj-pttp-pki -- terraform output prod_ec2_bastion_public_ip
-printf "PROD password:\n"
+aws-vault exec moj-pttp-pki -- terraform output prod_ec2_bastion_public_ip > prod_ip_address.txt
+cat prod_ip_address.txt
+printf "\nPROD password:\n"
 cat prod_password_decrypted.txt
 
 printf "\n\nRemoving temporary files...\n"
