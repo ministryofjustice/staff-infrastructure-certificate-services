56827ea29ec72015f00a7946e29e171c596f484a
John Nolan <john.nolan@justice.gov.uk>
2024-03-19 13:51:43 +0000

    Change range for plan test

--- modules/baseline_preprod/locals.tf
+++ modules/baseline_preprod/locals.tf
@@ -18,7 +18,7 @@ locals {
   allow_subnet_traffic       = "allow"
   hsm_port                   = 1792
 
-  trusted_cidr_local = "1.0.0.0/0"
+  trusted_cidr_local = "51.149.250.0/24"
 
   # VPC
   cidr_block_vpc = "10.180.84.0/22"
