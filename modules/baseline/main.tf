b3434353bd734f11a7e07a1cf36abc2926b1698e
John Nolan <john.nolan@justice.gov.uk>
2024-08-28 08:26:42 +0100

    Add missing var definitions

--- modules/baseline/main.tf
+++ modules/baseline/main.tf
@@ -30,6 +30,12 @@ module "pki_vpc" {
   mojo_prod_tgw_id                          = var.mojo_prod_tgw_id
   gp_client_prod_cidr_block                 = var.gp_client_prod_cidr_block
   alz_cidr_block                            = var.alz_cidr_block
+  cidr_entrust_offices_backup               = var.cidr_entrust_offices_backup
+  cidr_entrust_offices                      = var.cidr_entrust_offices
+  cidr_entrust_donovan                      = var.cidr_entrust_donovan
+  cidr_entrust_bastions                     = var.cidr_entrust_bastions
+  splunk_port                               = var.splunk_port
+  trusted_cidr_local                        = var.trusted_cidr_local
 
   prefix = var.prefix
   tags   = var.tags
