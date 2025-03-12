83102c0cfc2a6efc1b95be47e328f3d732270bc9
John Nolan <john.nolan@justice.gov.uk>
2025-03-12 16:09:07 +0000

    Add new vpn module

---
+++ modules/vpnv2/main.tf
@@ -0,0 +1,53 @@
+resource "aws_vpn_gateway" "vpn_gateway_v2" {
+  tags = {
+    "Name" = "${var.prefix}-pki-vpgv2"
+  }
+
+  vpc_id = var.vpc_id
+}
+
+### Tertiary
+resource "aws_vpn_connection" "vpn_tertiary" {
+  tags = {
+    "Name" = "${var.prefix}-pki-vpn-tertiary"
+  }
+
+  vpn_gateway_id      = aws_vpn_gateway.vpn_gateway_v2.id
+  customer_gateway_id = var.cgw_hsm_tertiary_id
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
+
+resource "aws_vpn_connection_route" "entrust" {
+  destination_cidr_block = var.tertiary_remote_destination_cidr
+  vpn_connection_id      = aws_vpn_connection.vpn_tertiary.id
+}
+
+resource "aws_route" "tertiary_vpn_route" {
+  route_table_id         = var.backend_zone_route_table_id
+  destination_cidr_block = var.tertiary_remote_destination_cidr
+  gateway_id             = aws_vpn_gateway.vpn_gateway_v2.id
+}
+
+### Quarternary
+resource "aws_vpn_connection" "vpn_quarternary" {
+  tags = {
+    "Name" = "${var.prefix}-pki-vpn-quarternary"
+  }
+
+  vpn_gateway_id      = aws_vpn_gateway.vpn_gateway_v2.id
+  customer_gateway_id = var.cgw_hsm_quarternary_id
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
+
+resource "aws_vpn_connection_route" "entrust" {
+  destination_cidr_block = var.quarternary_remote_destination_cidr
+  vpn_connection_id      = aws_vpn_connection.vpn_quarternary.id
+}
+
+resource "aws_route" "quarternary_vpn_route" {
+  route_table_id         = var.backend_zone_route_table_id
+  destination_cidr_block = var.quarternary_remote_destination_cidr
+  gateway_id             = aws_vpn_gateway.vpn_gateway_v2.id
+}
