b1e95ddeb84400458df2b1ce0546561c8102fe29
John Nolan <john.nolan@justice.gov.uk>
2025-03-19 12:58:24 +0000

    Add new vars to root var file

--- variables.tf
+++ variables.tf
@@ -32,11 +32,35 @@ variable "seondary_internal_cidr" {
   type = string
 }
 
+variable "tertiary_remote_destination_cidr" {
+  type = string
+}
+
+variable "tertiary_internal_cidr" {
+  type = string
+}
+
+variable "quarternary_remote_destination_cidr" {
+  type = string
+}
+
+variable "quarternary_internal_cidr" {
+  type = string
+}
+
 variable "cgw_hsm_primary_ip" {
   type = string
 }
 
 variable "cgw_hsm_secondary_ip" {
+  type = string
+}
+
+variable "cgw_hsm_tertiary_id" {
+  type = string
+}
+
+variable "cgw_hsm_quarternary_id" {
   type = string
 }
 
