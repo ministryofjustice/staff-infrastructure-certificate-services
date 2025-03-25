33e2a8bac96577b45200c1539ad1cba3c5d0244e
John Nolan <john.nolan@justice.gov.uk>
2025-03-25 16:15:36 +0000

    Remove refactor comments

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -43,13 +43,13 @@ resource "aws_vpn_connection" "vpn_secondary" {
 }
 
 resource "aws_vpn_connection_route" "entrust_secondary" {
-  destination_cidr_block = var.secondary_remote_destination_cidr //192.168.30.0/24
+  destination_cidr_block = var.secondary_remote_destination_cidr
   vpn_connection_id      = aws_vpn_connection.vpn_secondary.id
 }
 
 resource "aws_route" "secondary_vpn_route" {
   route_table_id         = var.backend_zone_route_table_id
-  destination_cidr_block = var.secondary_remote_destination_cidr //192.168.30.0/24
+  destination_cidr_block = var.secondary_remote_destination_cidr
   gateway_id             = aws_vpn_gateway.vpn_gateway.id
 }
 
@@ -60,19 +60,19 @@ resource "aws_vpn_connection" "vpn_ld6" {
   }
 
   vpn_gateway_id      = aws_vpn_gateway.vpn_gateway.id
-  customer_gateway_id = var.cgw_hsm_ld6_id // 88.84.131.132 
+  customer_gateway_id = var.cgw_hsm_ld6_id
   type                = "ipsec.1"
   static_routes_only  = true
 }
 
 resource "aws_vpn_connection_route" "entrust" {
-  destination_cidr_block = var.ld6_remote_destination_cidr //[192.168.31.0/24, 192.168.3.0/24]
+  destination_cidr_block = var.ld6_remote_destination_cidr
   vpn_connection_id      = aws_vpn_connection.vpn_ld6.id
 }
 
 resource "aws_route" "ld6_vpn_route" {
   route_table_id         = var.backend_zone_route_table_id
-  destination_cidr_block = var.ld6_remote_destination_cidr //[192.168.31.0/24, 192.168.3.0/24]
+  destination_cidr_block = var.ld6_remote_destination_cidr
   gateway_id             = aws_vpn_gateway.vpn_gateway.id
 }
 
@@ -83,18 +83,18 @@ resource "aws_vpn_connection" "vpn_tsc" {
   }
 
   vpn_gateway_id      = aws_vpn_gateway.vpn_gateway.id
-  customer_gateway_id = var.cgw_hsm_tsc_id // 213.1.236.32 // Rename LD6 and TSC
+  customer_gateway_id = var.cgw_hsm_tsc_id
   type                = "ipsec.1"
   static_routes_only  = true
 }
 
 resource "aws_vpn_connection_route" "entrust" {
-  destination_cidr_block = var.tsc_remote_destination_cidr //[192.168.41.0/24, 192.168.13.0/24]
+  destination_cidr_block = var.tsc_remote_destination_cidr
   vpn_connection_id      = aws_vpn_connection.vpn_tsc.id
 }
 
 resource "aws_route" "tsc_vpn_route" {
   route_table_id         = var.backend_zone_route_table_id
-  destination_cidr_block = var.tsc_remote_destination_cidr //[192.168.41.0/24, 192.168.13.0/24]
+  destination_cidr_block = var.tsc_remote_destination_cidr
   gateway_id             = aws_vpn_gateway.vpn_gateway.id
 }
