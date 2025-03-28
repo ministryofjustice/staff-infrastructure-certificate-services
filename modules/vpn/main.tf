f2e80181febce3a241b91f19d2e332179905766a
John Nolan <john.nolan@justice.gov.uk>
2025-03-28 09:32:30 +0000

    Add phase 2 tunnel settings

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -68,11 +68,17 @@ resource "aws_vpn_connection" "vpn_ld6" {
   tunnel1_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
   tunnel1_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
   tunnel1_ike_versions                 = ["ikev2"]
+  tunnel1_phase2_encryption_algorithms = ["AES256"]
+  tunnel1_phase2_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel1_phase2_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
 
   tunnel2_phase1_encryption_algorithms = ["AES256"]
   tunnel2_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
   tunnel2_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
   tunnel2_ike_versions                 = ["ikev2"]
+  tunnel2_phase2_encryption_algorithms = ["AES256"]
+  tunnel2_phase2_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel2_phase2_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
 }
 
 resource "aws_vpn_connection_route" "entrust_ld6" {
@@ -101,11 +107,17 @@ resource "aws_vpn_connection" "vpn_tsc" {
   tunnel1_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
   tunnel1_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
   tunnel1_ike_versions                 = ["ikev2"]
+  tunnel1_phase2_encryption_algorithms = ["AES256"]
+  tunnel1_phase2_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel1_phase2_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
 
   tunnel2_phase1_encryption_algorithms = ["AES256"]
   tunnel2_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
   tunnel2_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
   tunnel2_ike_versions                 = ["ikev2"]
+  tunnel2_phase2_encryption_algorithms = ["AES256"]
+  tunnel2_phase2_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel2_phase2_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
 }
 
 resource "aws_vpn_connection_route" "entrust_tsc" {
