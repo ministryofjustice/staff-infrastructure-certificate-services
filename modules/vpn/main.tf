1b867a05950ace6fde93cc51864a4853b184d436
John Nolan <john.nolan@justice.gov.uk>
2025-03-28 09:24:57 +0000

    Change the variable types

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -64,15 +64,15 @@ resource "aws_vpn_connection" "vpn_ld6" {
   type                = "ipsec.1"
   static_routes_only  = true
 
-  tunnel1_phase1_encryption_algorithms = "AES256"
-  tunnel1_phase1_dh_group_numbers      = "20 | 21 | 22 | 23 | 24"
-  tunnel1_phase1_integrity_algorithms  = "SHA2-256 | SHA2-384 | SHA2-512"
-  tunnel1_ike_versions                 = "ikev2"
+  tunnel1_phase1_encryption_algorithms = ["AES256"]
+  tunnel1_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel1_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
+  tunnel1_ike_versions                 = ["ikev2"]
 
-  tunnel2_phase1_encryption_algorithms = "AES256"
-  tunnel2_phase1_dh_group_numbers      = "20 | 21 | 22 | 23 | 24"
-  tunnel2_phase1_integrity_algorithms  = "SHA2-256 | SHA2-384 | SHA2-512"
-  tunnel2_ike_versions                 = "ikev2"
+  tunnel2_phase1_encryption_algorithms = ["AES256"]
+  tunnel2_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel2_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
+  tunnel2_ike_versions                 = ["ikev2"]
 }
 
 resource "aws_vpn_connection_route" "entrust_ld6" {
@@ -97,15 +97,15 @@ resource "aws_vpn_connection" "vpn_tsc" {
   type                = "ipsec.1"
   static_routes_only  = true
 
-  tunnel1_phase1_encryption_algorithms = "AES256"
-  tunnel1_phase1_dh_group_numbers      = "20 | 21 | 22 | 23 | 24"
-  tunnel1_phase1_integrity_algorithms  = "SHA2-256 | SHA2-384 | SHA2-512"
-  tunnel1_ike_versions                 = "ikev2"
+  tunnel1_phase1_encryption_algorithms = ["AES256"]
+  tunnel1_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel1_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
+  tunnel1_ike_versions                 = ["ikev2"]
 
-  tunnel2_phase1_encryption_algorithms = "AES256"
-  tunnel2_phase1_dh_group_numbers      = "20 | 21 | 22 | 23 | 24"
-  tunnel2_phase1_integrity_algorithms  = "SHA2-256 | SHA2-384 | SHA2-512"
-  tunnel2_ike_versions                 = "ikev2"
+  tunnel2_phase1_encryption_algorithms = ["AES256"]
+  tunnel2_phase1_dh_group_numbers      = ["20", "21", "22", "23", "24"]
+  tunnel2_phase1_integrity_algorithms  = ["SHA2-256", "SHA2-384", "SHA2-512"]
+  tunnel2_ike_versions                 = ["ikev2"]
 }
 
 resource "aws_vpn_connection_route" "entrust_tsc" {
